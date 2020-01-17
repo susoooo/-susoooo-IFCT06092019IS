66a9eed156f1d954a220e55124a5155b6f5d7999
AlexFerLo <alexferlo86@gmail.com>
2020-01-17 20:05:31 +0100

    mas ejercicios de procesos

---
+++ ALEX/procesos2.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+pid_t rf;
+int hijos;
+int contador;
+int main(int argc,char*argv[])
+{
+     scanf("%d",&hijos);
+     for (contador=0;contador<hijos;contador++)
+     {
+        rf = fork();
+        switch(rf)
+        {
+            case -1:
+                    printf("No he podido crear el proceso hijo \n");
+                    break;
+            case 0 :
+                    execv("./programa1.out",argv);
+                    printf("un hijo");
+                    break;
+            default:
+                    wait(0);
+        }
+
+
+     }printf("Soy el padre\n");
+      exit(0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ALEX/programa1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc,char*argv[])
+{
+    printf("A mamarla\n");
+    return (0);
+}
