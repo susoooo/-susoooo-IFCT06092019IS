41e578614a7c54a184ff71ae83438e05e68b0b66
malorenzoc <malorenzoc@malorenzoc.com>
2020-01-21 20:21:37 +0100

    Ejercicios Threads

---
+++ Repo_M_Lorenzo/Ejercicios_threads/Ejercicio1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void * thread_function(void *arg) 
+{
+    int i;
+    for (i=0;i<=2;i++) 
+    {
+        printf("Thread says hi!\n");
+        sleep(1);
+    }
+    printf("Thread exists\n");
+    return NULL;
+}
+
+int main(int argc, char ** argv) 
+{
+    pthread_t mythread;
+
+    printf("Launching new thread . . .\n");
+
+    if (pthread_create(&mythread,NULL,thread_function,NULL)!=0) 
+    {
+        printf("error creating thread.");
+        abort();
+    }
+    printf("Waiting on join\n");
+
+    pthread_join(mythread,NULL);
+
+    printf("Fin\n");
+
+    
+    exit(0);
+}
