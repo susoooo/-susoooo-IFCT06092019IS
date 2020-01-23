37317ff2f908f1d566c8575fe13c5142ff2054a0
nestorrois <restirui@gmail.com>
2020-01-23 20:27:19 +0100

    version 23/01

--- nestor/threads6.c
+++ nestor/threads6.c
@@ -6,11 +6,53 @@ cuantos threads se quieren lanzar para recorrer el archivo.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+//#include <sys/types.h>
+//#include <sys/stat.h>
+//#include <fcntl.h>
 
-void *funcionprint(void *rango); // necesitamos declarar funcionsuma antes do main, e sempre usamos (void *) con threads
+void * funcionprint(void * parametro); 
 int main()
   {
-  pthread_t threadprint;
+  pthread_t threadprint[50];
+  int numthreads;
+  int parametro;
+  int contador;
+
+  numthreads=0;
+  contador =0;
+  
+  printf("¿cuantos threads quieres crear para imprimir? maximo 50 = ");
+  scanf("%d",&numthreads);
+  for (contador=0;contador<numthreads;contador++)
+      {
+       pthread_create(&threadprint[contador],NULL,funcionprint,NULL);
+       pthread_join(threadprint,(void *)&parametro);
+      }
+  }
+void * funcionprint(void * parametro)
+      {
+       FILE *manejadorarchivo;
+       char *espaciomemoria;
+       fflush(stdout);
+       manejadorarchivo=fopen("/home/nestor/Documentos/repositorionestor/IFCT06092019IS/nestor/miarchivo.txt","rt");
+       if(manejadorarchivo==0)
+         {
+         perror("error o leer o archivo");   
+         }
+          else
+              {
+              espaciomemoria=malloc(sizeof(char));
+              do 
+                {
+                fread(espaciomemoria,sizeof(char),1,manejadorarchivo);
+                printf("%c",*espaciomemoria); 
+                }
+              while(!feof(manejadorarchivo));
+              fclose(manejadorarchivo);
+              free(espaciomemoria);
+              
+              pthread_exit(NULL);
+              } 
+      return(0);      
+      }
+  
