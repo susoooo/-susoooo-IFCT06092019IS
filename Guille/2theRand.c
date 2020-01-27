b525260fc51b096e2121ab4cc8a23bb261333eb7
XVili <xguirei@gmail.com>
2020-01-27 20:07:10 +0100

    Exercicio de 2 theads con un rand modificado

--- Guille/2theRand.c
+++ Guille/2theRand.c
@@ -2,29 +2,31 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <unistd.h>
-int elementos[100];
-pthread1=thread1;
-pthread2=thread2;
+int elementos[100000000];
+pthread_t thread1;
+pthread_t thread2;
+
 void * lector(void *arg)
 {
-int contador
+int contador;
 for(contador=0;contador<100;contador++)
    {
-	printf ("soy el thread1....elementos [contador]", elementos[contador]);
+	printf ("soy el thread1....,ID:     %d,pos:  ,valor:%d    \n",thread1,contador,  elementos[contador]);
    }
 }
 
 void * lector2(void *arg)
 {
-int contador
+int contador;
 for(contador=100;contador>=0;contador--)
    {
-	printf ("soy el thread2....elementos [contador]",elementos[contador]);
+	printf ("soy el thread2....,ID:     %d,pos:  ,valor:%d    \n",thread2,contador,  elementos[contador]);
    }
 }
 
 main()
- {
+   {
+       
    int contador;
   for(contador=0;contador<100;contador++)
   {
@@ -32,7 +34,8 @@ main()
   }
   pthread_create(&thread1,NULL,lector,NULL);
   pthread_create(&thread2,NULL,lector2,NULL);
-
+  pthread_join (thread1,NULL);
+  pthread_join (thread2,NULL);
  fflush (stdout);
  exit(0);
- }
+  }
