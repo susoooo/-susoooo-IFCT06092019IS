cfca0e1b3bba3c1807e3b515800f5abe31f3e504
Victor <victormsp@yahoo.es>
2020-02-27 20:17:06 +0100

    Versión 11.2 ejercicios herencias c++

---
+++ Victor/c++/herencia/inmobiliaria.cpp
@@ -0,0 +1,249 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Local
+{
+    protected:
+    char direccion[50];
+    float metros;
+    float precio;
+    public:
+    Local (char *d, float m, float p);
+    void Cambiardireccion(char *d);
+    int Verdireccion();
+    void Imprimelocal ();
+};
+
+Local::Local (char *d, float m, float p)
+{
+    strcpy(direccion, d);
+    metros=m;
+    precio=p;
+}
+
+void Local::Cambiardireccion (char *d)
+{
+    strcpy(direccion, d);
+}
+
+int Local::Verdireccion ()
+{
+   return *direccion;
+}
+
+void Local::Imprimelocal ()
+{
+   cout << direccion << endl
+        << metros << " " << precio << endl << endl;
+}
+
+class Tienda : public Local
+{
+    protected:
+    float mfachada;
+    public:
+    Tienda (char *d, float m, float p, float mf):Local(d, m, p)
+    {
+        mfachada=mf;
+    }
+    void Cambiarmfachada(float mf);
+    int Vermfachada();
+    void Imprimetienda ();
+};
+
+class Hosteleria : public Local
+{
+    protected:
+    float tiempoabierto;
+    public:
+    Hosteleria (char *d, float m, float p, float ta):Local(d, m, p)
+    {
+        tiempoabierto=ta;
+    }
+    void Cambiartiempoabierto(float mf);
+    int Vertiempoabierto();
+    void Imprimehosteleria ();
+};
+
+class Tiendaropa : public Tienda
+{
+    protected:
+    bool probadores;
+    bool aseos;
+    public:
+    Tiendaropa (char *d, float m, float p, float mf, bool pr, bool as):Tienda(d, m, p, mf)
+    {
+        probadores=pr;
+        aseos=as;
+    }
+    void Cambiarprobadores(bool pr);
+    int Verprobadores();
+    void Imprimetiendaropa ();
+};
+
+class Tiendacomida : public Tienda
+{
+    protected:
+    int liniales;
+    int cajas;
+    public:
+    Tiendacomida (char *d, float m, float p, float mf, int li, int ca):Tienda(d, m, p, mf)
+    {
+        liniales=li;
+        cajas=ca;
+    }
+    void Cambiarcajas(int ca);
+    int Vercajas();
+    void Imprimetiendacomida ();
+};
+
+class Tiendaferreteria : public Tienda
+{
+    protected:
+    float malmacen;
+    public:
+    Tiendaferreteria (char *d, float m, float p, float mf, float ma):Tienda(d, m, p, mf)
+    {
+        malmacen=ma;
+    }
+    void Cambiarmalmacen(float ma);
+    int Vermalmacen();
+    void Imprimetiendaferreteria ();
+};
+
+class Tiendadrogueria : public Tienda
+{
+    protected:
+    bool matpeligrosos;
+    public:
+    Tiendadrogueria (char *d, float m, float p, float mf, bool mp):Tienda(d, m, p, mf)
+    {
+        matpeligrosos=mp;
+    }
+    void Cambiarmatpeligrosos(bool mp);
+    int Vermatpeligrosos();
+    void Imprimetiendadrogueria ();
+};
+
+class Hosteleriabar : public Hosteleria
+{
+    protected:
+    int nbarras;
+    public:
+    Hosteleriabar (char *d, float m, float p, float ta, int nb):Hosteleria(d, m, p, ta)
+    {
+        nbarras=nb;
+    }
+    void Cambiarnbarras(int nb);
+    int Vernbarras();
+    void Imprimehosteleriabar ();
+};
+
+class Hosteleriarestaurante : public Hosteleria
+{
+    protected:
+    int nmesas;
+    float mcomedor;
+    float mcocina;
+    public:
+    Hosteleriarestaurante (char *d, float m, float p, float ta, int nm, float com, float coc):Hosteleria(d, m, p, ta)
+    {
+        nmesas=nm;
+        mcomedor=com;
+        mcocina=coc;
+    }
+    void Cambiarnmesas(int nm);
+    int Vernmesas();
+    void Imprimehosteleriarestaurante ();
+};
+
+class Hosteleriacomidallevar : public Hosteleria
+{
+    protected:
+    bool cocina;
+    bool pevacuacion;
+    public:
+    Hosteleriacomidallevar (char *d, float m, float p, float ta, bool coc, bool pe):Hosteleria(d, m, p, ta)
+    {
+        cocina=coc;
+        pevacuacion=pe;
+    }
+    void Cambiarcocina(bool coc);
+    int Vercocina();
+    void Imprimehosteleriacomidallevar ();
+};
+
+class Tiendaropacamiseria : public Tiendaropa
+{
+    protected:
+    bool amedida;
+    public:
+    Tiendaropacamiseria (char *d, float m, float p, float mf, bool pr, bool as, bool am):Tiendaropa(d, m, p, mf, pr, as)
+    {
+        amedida=am;
+    }
+};
+
+class Tiendaropamerceria : public Tiendaropa
+{
+    protected:
+    int nmostradores;
+    public:
+    Tiendaropamerceria (char *d, float m, float p, float mf, bool pr, bool as, int nm):Tiendaropa(d, m, p, mf, pr, as)
+    {
+        nmostradores=nm;
+    }
+};
+
+class Tiendaropasombrereria : public Tiendaropa
+{
+    protected:
+    int nexpositores;
+    public:
+    Tiendaropasombrereria (char *d, float m, float p, float mf, bool pr, bool as, int ne):Tiendaropa(d, m, p, mf, pr, as)
+    {
+        nexpositores=ne;
+    }
+};
+
+class Tiendacomidafruteria : public Tiendacomida
+{
+    protected:
+    bool cfrigorifica;
+    public:
+    Tiendacomidafruteria (char *d, float m, float p, float mf, int li, int ca, bool cf):Tiendacomida(d, m, p, mf, li, ca)
+    {
+        cfrigorifica=cf;
+    }
+};
+
+class Tiendacomidacarniceria : public Tiendacomida
+{
+    protected:
+    int nfrigorifico;
+    public:
+    Tiendacomidacarniceria (char *d, float m, float p, float mf, int li, int ca, int nf):Tiendacomida(d, m, p, mf, li, ca)
+    {
+        nfrigorifico=nf;
+    }
+};
+
+class Tiendacomidapanaderia : public Tiendacomida
+{
+    protected:
+    int narmarios;
+    bool scafeteria;
+    public:
+    Tiendacomidapanaderia (char *d, float m, float p, float mf, int li, int ca, int na, bool sc):Tiendacomida(d, m, p, mf, li, ca)
+    {
+        narmarios=na;
+        scafeteria=sc;
+    }
+};
+
+int main()
+{
+
+}
--- Victor/c++/herencia/persona.cpp
+++ Victor/c++/herencia/persona.cpp
@@ -17,6 +17,8 @@ class ClasePersona
     char paisnac[30];
     public:
     ClasePersona (long d, char *n, char *a1, char *a2, int dn, int mn, int an, char *l, char *p);
+    void Cambiardni(long d);
+    int Verdni();
     void Imprimedatos ();
 };
 
@@ -32,6 +34,17 @@ ClasePersona::ClasePersona (long d, char *n, char *a1, char *a2, int dn, int mn,
     strcpy(lugarnac, l);
     strcpy(paisnac, p);
 }
+
+void ClasePersona::Cambiardni (long d)
+{
+    dni=000000000;
+}
+
+int ClasePersona::Verdni ()
+{
+   return dni;
+}
+
 void ClasePersona::Imprimedatos ()
 {
    cout << nombre << " " << ape1 << " " << ape2 << endl
@@ -42,38 +55,106 @@ void ClasePersona::Imprimedatos ()
 
 class Asalariado : public ClasePersona
 {
+    protected:
     float sueldo;
     float retencion;
     public:
-    Asalariado (long d, char *n, char *a1, char *a2, int dn, int mn, int an, char *l, char *p, float s, float r);
-    void Imprimedatos ();
+    Asalariado (long d, char *n, char *a1, char *a2, int dn, int mn, int an, char *l, char *p, float s, float r):ClasePersona(d, n, a1, a2, dn, mn, an, l, p)
+    {
+        sueldo=s;
+        retencion=r;
+    }
+    void Cambiarsueldo(float s);
+    int Versueldo();
+    void Imprimeasalariado ();
 };
 
-Asalariado::Asalariado (long d, char *n, char *a1, char *a2, int dn, int mn, int an, char *l, char *p, float s, float r)
+void Asalariado::Cambiarsueldo (float s)
 {
-    dni=d;
-    strcpy(nombre, n);
-    strcpy(ape1, a1);
-    strcpy(ape2, a2);
-    dianac=dn;
-    mesnac=mn;
-    anonac=an;
-    strcpy(lugarnac, l);
-    strcpy(paisnac, p);
-    sueldo=s;
-    retencion=r;
+    sueldo=0.0;
 }
 
-void Asalariado::Imprimedatos ()
+int Asalariado::Versueldo ()
 {
-   cout << nombre << " " << ape1 << " " << ape2 << endl
-        << dni << endl
-        << dianac << "/" << mesnac << "/" << anonac << endl
-        << lugarnac << " " << paisnac << endl
-        << sueldo << " " << retencion << endl << endl;
+   return sueldo;
+}
+
+void Asalariado::Imprimeasalariado ()
+{
+    Imprimedatos ();
+    cout << sueldo << " " << retencion << endl << endl;
+}
+
+class Obrero : public Asalariado
+{
+    char cargo[30];
+    public:
+    Obrero (long d, char *n, char *a1, char *a2, int dn, int mn, int an, char *l, char *p, float s, float r, char *c):Asalariado(d, n, a1, a2, dn, mn, an, l, p, s, r)
+    {
+        strcpy(cargo, c);
+    }
+    void Cambiarcargo(char *c);
+    int Vercargo();
+    void Imprimeobrero ();
+};
+
+void Obrero::Cambiarcargo (char *c)
+{
+    strcpy(cargo, c);
+}
+
+int Obrero::Vercargo ()
+{
+   return *cargo;
+}
+
+void Obrero::Imprimeobrero ()
+{
+    Imprimeasalariado ();
+    cout << cargo << endl << endl;
+}
+
+class Medico : public Asalariado
+{
+    int npacientes;
+    public:
+    Medico (long d, char *n, char *a1, char *a2, int dn, int mn, int an, char *l, char *p, float s, float r, int np):Asalariado(d, n, a1, a2, dn, mn, an, l, p, s, r)
+    {
+        npacientes=np;
+    }
+    void Cambiarnpacientes(int np);
+    int Vernpacientes();
+    void Imprimemedico ();
+};
+
+void Medico::Cambiarnpacientes (int np)
+{
+    npacientes=np;
+}
+
+int Medico::Vernpacientes ()
+{
+   return npacientes;
+}
+
+void Medico::Imprimemedico ()
+{
+    Imprimeasalariado ();
+    cout << npacientes << endl << endl;
 }
 
 int main()
 {
-
+    ClasePersona Victor (33445577,"Víctor","Sánchez","Penas", 1, 1, 1980,"Lugo","España");
+    Victor.Imprimedatos();
+    Asalariado Pepe (11223344,"Pepe","Pérez","Pérez", 3, 1, 1985,"Lugo","España",1000,0.10);
+    Pepe.Imprimeasalariado();
+    Asalariado Jose (44223311,"José","Gómez","Pérez", 10, 10, 1981,"Madrid","España",1100,0.12);
+    Jose.Imprimeasalariado();
+    Asalariado Manuel (55882211,"Manuel","García","García", 13, 11, 1983,"Ourense","España",1100,0.10);
+    Manuel.Imprimeasalariado();
+    Obrero Juan (55554444,"Juan","Álvarez","Gómez", 4, 5, 1982,"A Coruña","España",900,0.06, "Peón");
+    Juan.Imprimeobrero();
+    Medico Antonio (11223344,"Antonio","Martínez","García", 15, 12, 1978,"León","España",1300,0.15,24);
+    Antonio.Imprimemedico();
 }
