d2ac857f230b2f2337fa2ee62a4321680d4529e9
AlexFerLo <alexferlo86@gmail.com>
2020-07-10 19:52:18 +0200

    Programando objetos en JAVA

---
+++ ALEX/Java/punto/src/punto/Punto.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coord;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alex
+ */
+public class Punto {
+
+   int x;
+   int y;
+   
+   public int getX()
+   { 
+       return  (x);
+   }
+   public void setX(int x)
+   { this.x = x;
+        
+   }
+   
+    public int getY()
+   { 
+       return (y);
+   }
+   public void setY(int y)
+   { this.y = y;
+   
+   }
+
+}
+         class Coord {
+    public static void main(String[] args) {
+    Punto p1;
+    Punto p2;
+    Punto p3;
+    
+   
+    
+    p1 = new Punto();
+    p2 = new Punto();
+    p3 = new Punto();
+           
+    }
+    
+}
