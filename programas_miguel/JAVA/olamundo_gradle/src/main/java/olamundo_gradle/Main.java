a84e92e087451f59316a1b84c4a983239bfd5742
miguel1978diaz <m78sancovade@gmail.com>
2020-07-17 19:17:16 +0200

    Engado últimos exercicios de JAVA

---
+++ programas_miguel/JAVA/olamundo_gradle/src/main/java/olamundo_gradle/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package olamundo_gradle;
+
+/**
+ *
+ * @author ubuntu1
+ */
+public class Main 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+   public static void main(String[] args) 
+    {
+        System.out.println("Ola mundo");
+        // TODO code application logic here
+    }
+    
+}
