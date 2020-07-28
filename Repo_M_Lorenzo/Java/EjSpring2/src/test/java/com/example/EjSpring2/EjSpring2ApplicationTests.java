8c75b0df114f2e11f23eee71952e7df4f348a8a1
malorenzoc <malorenzoc@gmail.com>
2020-07-28 20:18:59 +0200

    Repositorio 28/07/2020 primeras aplicaciones web con spring

---
+++ Repo_M_Lorenzo/Java/EjSpring2/src/test/java/com/example/EjSpring2/EjSpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EjSpring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjSpring2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
