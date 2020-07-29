0884981c0adeabd5243d78b82a9a95c93b747700
nestorrois <restirui@gmail.com>
2020-07-29 19:22:11 +0200

    actualizacion repositorio

---
+++ nestor/java/hello/src/hello/Hello.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ *
+ * @author nestor
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+       System.out.println("Hola mundo"); // TODO code application logic here
+    }
+    
+}
+//public class Hello 
+//{
+ // public static void main(String[] args) 
+ // {
+    
+ // }
+//}
